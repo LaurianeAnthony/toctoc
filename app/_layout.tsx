04fafbb680ad177d52b1e51996bcbe7b736504be
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2025-08-11 09:31:47 +0200

    feat: Update SQLite initialization to drop and recreate checklist instances table with new seeding

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,30 +10,6 @@ import "react-native-reanimated";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
 
-const DATABASE_VERSION = 1;
-
-async function createTables(db: SQLiteDatabase, databaseVersion: number) {
-  // Create checklists table
-  await db.execAsync(`
-    PRAGMA journal_mode = 'wal';
-    CREATE TABLE checklist_instances (
-        id INTEGER PRIMARY KEY NOT NULL,
-        title TEXT NOT NULL,
-        createdAt TEXT NOT NULL DEFAULT (datetime('now')),
-        updatedAt TEXT NOT NULL DEFAULT (datetime('now'))
-    );
-  `);
-
-  // Seed some data
-  await db.execAsync(`
-    INSERT INTO checklist_instances (id, title, orderIndex) VALUES
-    (1, 'Départ de la maison', 1),
-    (2, 'Checkliste de voyage', 2);
-  `);
-
-  await db.execAsync(`PRAGMA user_version = ${databaseVersion}`);
-}
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
@@ -45,19 +21,34 @@ export default function RootLayout() {
     return null;
   }
 
-  const migrateDb = async (db: SQLiteDatabase): Promise<void> => {
-    const dbInfo = await db.getFirstAsync<{
-      user_version: number;
-    }>("PRAGMA user_version");
-    console.log(dbInfo);
-    if (!dbInfo || dbInfo.user_version === 0) {
-      return await createTables(db, DATABASE_VERSION);
-    }
+  const initDb = async (db: SQLiteDatabase): Promise<void> => {
+    // Drop checklists table
+    await db.execAsync(`
+        DROP TABLE IF EXISTS checklist_instances
+    `);
+
+    // Create checklists table
+    await db.execAsync(`
+      PRAGMA journal_mode = 'wal';
+      CREATE TABLE IF NOT EXISTS checklist_instances (
+        id INTEGER PRIMARY KEY NOT NULL,
+        title TEXT NOT NULL,
+        createdAt TEXT NOT NULL DEFAULT (datetime('now')),
+        updatedAt TEXT NOT NULL DEFAULT (datetime('now'))
+      );
+    `);
+
+    // Seed some data
+    await db.execAsync(`
+      INSERT INTO checklist_instances (id, title) VALUES
+      (1, 'Départ de la maison'),
+      (2, 'Checkliste de voyage');
+    `);
   };
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <SQLiteProvider databaseName="toctoc.db" onInit={migrateDb}>
+      <SQLiteProvider databaseName="toctoc.db" onInit={initDb}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen
